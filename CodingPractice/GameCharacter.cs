6f62e8e8f3bc939c80bfb49164a1b8ab278536b1
garidana-collab <garidana@gmail.com>
2026-03-04 14:51:06 +0900

    31 Coding Practice
    almost done

--- CodingPractice/GameCharacter.cs
+++ CodingPractice/GameCharacter.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Console;
 
 abstract class GameCharacter
 {
     public string Name { get; protected set; }
     public int Health { get; protected set; }
+    
 
+    // 게임 캐릭터 생성자
+    public GameCharacter(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+    
     public abstract void Attack();
-    public abstract void ShowStatus();
+    public void ShowStatus()
+    {
+        WriteLine($"[{Name}] 체력: {Health}");
+    }
+    
 }
 
 class Warrior : GameCharacter
 {
-    
+    public int Strength;
+    public Warrior(int strength)
+    {
+        Strength = strength;
+    }
+    public void Attack()
+    {
+
+    }
 }
 
 class Mage : GameCharacter
